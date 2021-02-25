579e7e999fcb9bc6666640096edf71ebd8364335
ChristopherG19 <60325784+ChristopherG19@users.noreply.github.com>
2021-02-24 21:15:42 -0600

    Enter

---
+++ SingleCalcu.java
@@ -0,0 +1,16 @@
+/**
+ * UVG-Algoritmos y Estructura de datos-Seccion 10
+ * Catedrático: Douglas Barrios
+ * @author Christopher García, 20541
+ * @author Maria Fernanda Argueta, 20458 
+ * @version 1
+ */
+
+public class SingleCalcu {
+
+    private static SingleCalcu One_instance = null;
+
+
+
+
+}
